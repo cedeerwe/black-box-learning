4a04b99bf538ba74ab21381d29c9b7fd02946023
cd <cdominik@gmail.com>
2021-03-14 13:33:31 +0100

    Guessing game as a struct
    Move the whole logic on a struct, which starts the refactor to make
    the guessing game testable.

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,52 @@ use std::io;
 // will start guessing and the program answers with "Too small!" or "Too big!" until the player
 // guesses correctly and the program finishes.
 // Implementation based on https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
-fn main() {
-    let mut rng = rand::thread_rng();
-    let number_to_guess = rng.gen_range(0..=100);
+struct GuessingGame {
+    min: u32,
+    max: u32,
+}
 
-    println!("Hello dear friend. Guess my secret number!");
-
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Input cannot be parsed as a positive integer, try again!");
-                continue;
-            }
-        };
-
-        match guess.cmp(&number_to_guess) {
-            std::cmp::Ordering::Equal => {
-                println!("You guessed correctly, the number is {}!", guess);
-                break;
-            }
-            std::cmp::Ordering::Greater => println!("The number {} is too big, try again!", guess),
-            std::cmp::Ordering::Less => println!("The number {} is too small, try again!", guess),
-        };
+impl GuessingGame {
+    fn new(min: u32, max: u32) -> Self {
+        GuessingGame { min, max }
     }
+
+    fn start(&self) {
+        let mut rng = rand::thread_rng();
+        let number_to_guess = rng.gen_range(self.min..=self.max);
+
+        println!("Hello dear friend. Guess my secret number!");
+
+        loop {
+            let mut guess = String::new();
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Input cannot be parsed as a positive integer, try again!");
+                    continue;
+                }
+            };
+
+            match guess.cmp(&number_to_guess) {
+                std::cmp::Ordering::Equal => {
+                    println!("You guessed correctly, the number is {}!", guess);
+                    break;
+                }
+                std::cmp::Ordering::Greater => {
+                    println!("The number {} is too big, try again!", guess)
+                }
+                std::cmp::Ordering::Less => {
+                    println!("The number {} is too small, try again!", guess)
+                }
+            };
+        }
+    }
+}
+
+fn main() {
+    GuessingGame::new(0, 100).start();
 }
