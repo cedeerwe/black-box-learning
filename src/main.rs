148d84451b05e8a1d7d9fd77537617517d7b84c6
cd <cdominik@gmail.com>
2021-03-14 12:34:15 +0100

    Guessing game -- initial
    We code up a simple guessing game directly in the main function.
    This is obviously a bad design, but we start with the obvious and
    then we will refactor it multiple times.
    
    The code is mirroring the initial example from the official Rust docs
    https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
    
    We added a dependency using `cargo add rand` using the utility
    cargo-edit.
    
    There is a whole lot of Enums used in the body, we recommend reading
    up on them.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rand::Rng;
+use std::io;
+
+// A simple guessing game. The program fixes a number between 0 and 100. The player
+// will start guessing and the program answers with "Too small!" or "Too big!" until the player
+// guesses correctly and the program finishes.
+// Implementation based on https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let number_to_guess = rng.gen_range(0..=100);
+
+    println!("Hello dear friend. Guess my secret number!");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Input cannot be parsed as a positive integer, try again!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&number_to_guess) {
+            std::cmp::Ordering::Equal => {
+                println!("You guessed correctly, the number is {}!", guess);
+                break;
+            }
+            std::cmp::Ordering::Greater => println!("The number {} is too big, try again!", guess),
+            std::cmp::Ordering::Less => println!("The number {} is too small, try again!", guess),
+        };
+    }
 }
